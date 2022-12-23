a329fb15b10d141fc306be765253f74ca7dc6514
Jiong Qi <jiongqipan@gmail.com>
2022-12-23 14:04:43 -0500

    Post started

--- reddit_clone/app/controllers/sessions_controller.rb
+++ reddit_clone/app/controllers/sessions_controller.rb
@@ -1,3 +1,37 @@
 class SessionsController < ApplicationController
+
+    def new
+        @user = User.new
+        render :new
+    end 
+    
+    def create
+        username = params[:user][:username]
+        password = params[:user][:password]
+        @user = User.find_by_credentials(username, password)
+
+        if @user
+            login(@user)
+            redirect_to user_url(@user)
+        else
+            @user = User.new(
+                username: params[:user][:username],
+                password: params[:user][:password]
+            )
+            flash.now[:errors] = ["Invalid Credentials. Please try again..."]
+            render :new 
+        end
+    end
+
+    def destroy
+        if logged_in?
+            logout!
+        end
+
+        flash[:messages] = ["Successfully logged out!"]
+
+        redirect_to new_session_url
+    end
+    
     
 end
