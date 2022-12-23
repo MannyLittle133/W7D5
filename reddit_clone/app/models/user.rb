c4842506969d1a9e6e5788f1c111c1e1d7036610
Emmanuel Little <109988822+MannyLittle133@users.noreply.github.com>
2022-12-23 11:20:36 -0500

    models and apps

--- reddit_clone/app/models/user.rb
+++ reddit_clone/app/models/user.rb
@@ -35,5 +35,18 @@ class User < ApplicationRecord
         @password = password
     end
 
-    
+    def self.generate_session_token
+        self.session_token = SecureRandom::urlsafe_base64
+    end
+
+    def ensure_session_token
+        self.session_token ||= generate_session_token
+    end
+
+    def reset_session_token
+        self.session_token = generate_session_token
+        self.save!
+        self.session_token
+    end
+
 end
