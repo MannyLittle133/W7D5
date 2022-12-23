2be6d51bf27e738fd883c6ca20923a83cf3b8361
Emmanuel Little <109988822+MannyLittle133@users.noreply.github.com>
2022-12-23 13:46:27 -0500

    sessions

--- reddit_clone/app/controllers/subs_controller.rb
+++ reddit_clone/app/controllers/subs_controller.rb
@@ -18,5 +18,21 @@ class SubsController < ApplicationController
         @sub.moderator_id = current_user.id
         
         if @sub.save
-            redirect_to subs_url()
+            redirect_to subs_url(@sub)
+        else
+            flash.now[:errors] = @sub.errors.full_messages
+            render :new
+        end
+    end
+
+    def update 
+        @sub = Sub.find(params[:id])
+
+        if @sub.update(sub_params)
+            redirect_to sub_url(@sub)
+        else
+            render :json @sub.errors.full_message, status: :unprocessable_entity
+        end
+    end
+
 end
---
+++ reddit_clone/app/controllers/users_controller.rb
@@ -0,0 +1,48 @@
+class UsersController < ApplicationController
+
+    def index
+        @users = User.all
+        render :index
+    end
+
+    def show
+        @user = User.find_by(id: params[:id])
+        render :show
+    end
+
+    def new
+        @user = User.new
+        render :new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            login(@user)
+            redirect_to user_url(@user)
+        else
+            flash.now[:errors] = @users.errors.full_messages
+            render :new
+        end
+    end
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+
+    def edit 
+        @user = User.find(params[:id])
+        render :edit
+    end
+
+    def update
+        @user = User.find(params[:id])
+
+        if @user.update(user_params)
+            redirect_to user_url(@user)
+        else
+            render json: @user.errors.full_messages, status: 422
+        end
+    end
+
+end
